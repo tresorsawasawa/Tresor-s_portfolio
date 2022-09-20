b4b255ff1831be9bdf7ea143af9d86d875c82550
Tresor Sawasawa <tresorsawasawa@gmail.com>
2022-09-20 18:48:28 +0200

    Correct some typing errors and add the unnecessary images in ".gitignore" file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-
+import AOS from 'aos';
 import Header from './components/Header';
 import Home from './components/Home';
-import Portfolio from './components/Portfolio';
+import Work from './components/Work';
 import About from './components/About';
-import Testimonials from './components/Testimonials';
-import Contacts from './components/Contacts';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
 import Footer from './components/Footer';
 
+import './assets/css/main.css';
+import 'aos/dist/aos.css';
+
+AOS.init({
+  delay: 100,
+  duration: 800,
+});
 const App = () => (
   <>
-    <Header />
-    <main>
+    <main className="App">
+      <Header />
       <Home />
+      <Work />
       <Portfolio />
       <About />
-      <Testimonials />
-      <Contacts />
+      <Contact />
       <Footer />
     </main>
+    {/* <Loading /> */}
   </>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import './assets/scss/index.scss';
+import ReactGA from 'react-ga';
 import App from './App';
+import ScrollTop from './components/ScrollTop';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const TRACKING_ID = process.env.REACT_APP_GOOGLE_TRACKING_ID;
+ReactGA.initialize(TRACKING_ID);
+
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ScrollTop>
+        <App />
+      </ScrollTop>
     </BrowserRouter>
   </React.StrictMode>,
+  document.getElementById('root'),
 );
